11f86f382dd4551bea70384c9e036be2d9a200c8
Victor Machado <victor.machado.perez@gmail.com>
2023-07-09 20:54:08 +0200

    Build Login form

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,23 +1,86 @@
-import { Center, Grid, GridItem, Text } from "@chakra-ui/react";
-import NavBar from "./NavBar";
+import {
+  Flex,
+  Heading,
+  Input,
+  Button,
+  InputGroup,
+  Stack,
+  InputLeftElement,
+  chakra,
+  Box,
+  Link,
+  Avatar,
+  FormControl,
+  FormHelperText,
+} from "@chakra-ui/react";
+import { FaUserAlt, FaLock } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+
+const CFaUserAlt = chakra(FaUserAlt);
+const CFaLock = chakra(FaLock);
 
 const Login = () => {
+  const navigate = useNavigate();
   return (
-    <Grid
-      templateAreas={{
-        base: `"nav" "main"`,
-      }}
+    <Flex
+      flexDirection="column"
+      width="100wh"
+      height="100vh"
+      justifyContent="center"
+      alignItems="center"
     >
-      <GridItem area="nav">
-        <NavBar />
-      </GridItem>
-
-      <Center>
-        <GridItem area="main" width={700}>
-          <Text>Login</Text>
-        </GridItem>
-      </Center>
-    </Grid>
+      <Stack
+        flexDir="column"
+        mb="2"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Avatar bg="teal.500" />
+        <Heading color="teal.400" userSelect={"none"}>
+          Churripuntos
+        </Heading>
+        <Box minW={{ base: "90%", md: "468px" }}>
+          <form>
+            <Stack spacing={4} p="1rem" boxShadow="md">
+              <FormControl>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents="none"
+                    children={<CFaUserAlt color="gray.300" />}
+                  />
+                  <Input
+                    type="email"
+                    placeholder=" username or email address"
+                  />
+                </InputGroup>
+              </FormControl>
+              <FormControl>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents="none"
+                    color="gray.300"
+                    children={<CFaLock color="gray.300" />}
+                  />
+                  <Input type="password" placeholder="Password" />
+                </InputGroup>
+                <FormHelperText textAlign="right">
+                  <Link>Forgot password?</Link>
+                </FormHelperText>
+              </FormControl>
+              <Button type="submit" colorScheme="teal">
+                Login
+              </Button>
+            </Stack>
+          </form>
+        </Box>
+      </Stack>
+      <Box>
+        Not registered yet?{" "}
+        <Link color="teal.500" onClick={() => navigate("/register")}>
+          Sign Up
+        </Link>
+      </Box>
+    </Flex>
   );
 };
 
