b6d21e2a17a675871fc27594b68858e7c96e28ca
Victor Machado <victor.machado.perez@gmail.com>
2023-07-11 09:21:39 +0200

    Fetch user boards

--- src/components/Spaces.tsx
+++ src/components/Spaces.tsx
@@ -1,7 +1,9 @@
 import { Center, Grid, GridItem, Text } from "@chakra-ui/react";
 import NavBar from "./NavBar";
+import useSpaces from "../hooks/useSpaces";
 
 const Spaces = () => {
+  const { spaces, error } = useSpaces();
   return (
     <Grid
       templateAreas={{
@@ -14,7 +16,12 @@ const Spaces = () => {
 
       <Center>
         <GridItem area="main" width={700}>
-          <Text>Spaces</Text>
+          {error && <Text>{error}</Text>}
+          <ul>
+            {spaces.map((space) => (
+              <li key={space._id}>{space.spacename}</li>
+            ))}
+          </ul>
         </GridItem>
       </Center>
     </Grid>
