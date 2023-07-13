a6a1eafc557bc093e16a99f0d22356987ac4111e
Victor Machado <victor.machado.perez@gmail.com>
2023-07-13 18:37:28 +0200

    Added title to ranking list

--- src/components/Ranking.tsx
+++ src/components/Ranking.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import RankingCard from "./RankingCard";
-import { SimpleGrid } from "@chakra-ui/react";
+import { Heading, SimpleGrid } from "@chakra-ui/react";
 
 const Ranking = () => {
   return (
-    <SimpleGrid padding={5} spacing={1}>
+    <SimpleGrid spacing={1}>
+      <Heading size={"lg"}>RANKING</Heading>
       <RankingCard position={1} username="Victor" points={100} />
       <RankingCard position={2} username="Pau" points={100} />
     </SimpleGrid>
