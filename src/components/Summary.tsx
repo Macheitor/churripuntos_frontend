2d981a46a7143e3241bdf787a9b78d306d82f189
Victor Machado <victor.machado.perez@gmail.com>
2023-07-19 18:24:30 +0200

    dates in summary

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -12,6 +12,7 @@ import {
   ModalFooter,
   ModalBody,
   Button,
+  Stack,
 } from "@chakra-ui/react";
 import { Activity } from "./Space";
 import { CloseIcon } from "@chakra-ui/icons";
@@ -24,10 +25,58 @@ interface Props {
   onUpdateSpace: () => void;
 }
 
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 const Summary = ({ tasksDone, onUpdateSpace }: Props) => {
   const { onOpen, onClose, isOpen } = useDisclosure();
   const [deleteTaskDoneId, setDeleteTaskDoneId] = useState("");
   const [error, setError] = useState("");
+  let lastDate = "";
+
+  const getMyDate = (taskDateDB: string) => {
+    const currentDate = new Date();
+    const taskDate = new Date(taskDateDB);
+
+    if (
+      taskDate.getMonth() === currentDate.getMonth() &&
+      taskDate.getFullYear() === currentDate.getFullYear()
+    ) {
+      if (taskDate.getDay() === currentDate.getDay()) return "Today";
+      if (taskDate.getDay() === currentDate.getDay() - 1) return "Yesterday";
+    }
+
+    const day = taskDate.getDay();
+    const date = taskDate.getDate();
+    const month = taskDate.getMonth() + 1;
+    const year = taskDate.getFullYear();
+
+    const result = `${days[day].substring(0, 3)}, ${date} ${months[
+      month - 1
+    ].substring(0, 3)} ${year}
+  `;
+    return result;
+  };
 
   // Delete taskDone
   const deleteTaskDone = (taskDoneId: string) => {
@@ -55,34 +104,46 @@ const Summary = ({ tasksDone, onUpdateSpace }: Props) => {
       <Center>
         <Heading size={"lg"}>SUMMARY</Heading>
       </Center>
-      {tasksDone.map((t) => (
-        <Box w="100%" bg={"gray.700"} borderRadius={10} key={t._id}>
-          <HStack justify={"space-between"} p={2}>
-            <Text>{t.taskname}</Text>
-            <Text>{t.points} points</Text>
-            <Text>{t.username}</Text>
-            <Box
-              bg="gray.600"
-              border="3px"
-              borderRadius={20}
-              borderColor="gray.700"
-            >
-              <CloseIcon
-                fontSize={8}
-                mb={1}
-                mr={2}
-                ml={2}
-                color="gray.700"
-                onClick={(e) => {
-                  setDeleteTaskDoneId(t._id);
-                  onOpen();
-                  e.stopPropagation();
-                }}
-              />
+      {[...tasksDone].reverse().map((t) => {
+        let printDate = false;
+        // TODO: DO this right not using substring
+        if (t.date.substring(0,10) !== lastDate.substring(0,10)) {
+          lastDate = t.date;
+          printDate = true;
+        } 
+
+        return (
+          <Stack>
+            {printDate && <Heading size={"md"}>{getMyDate(t.date)}</Heading>}
+            <Box w="100%" bg={"gray.700"} borderRadius={10} key={t._id}>
+              <HStack justify={"space-between"} p={2}>
+                <Text>{t.taskname}</Text>
+                <Text>{t.points} points</Text>
+                <Text>{t.username}</Text>
+                <Box
+                  bg="gray.600"
+                  border="3px"
+                  borderRadius={20}
+                  borderColor="gray.700"
+                >
+                  <CloseIcon
+                    fontSize={8}
+                    mb={1}
+                    mr={2}
+                    ml={2}
+                    color="gray.700"
+                    onClick={(e) => {
+                      setDeleteTaskDoneId(t._id);
+                      onOpen();
+                      e.stopPropagation();
+                    }}
+                  />
+                </Box>
+              </HStack>
             </Box>
-          </HStack>
-        </Box>
-      ))}
+          </Stack>
+        );
+      })}
       <Modal
         isOpen={isOpen}
         onClose={() => {
