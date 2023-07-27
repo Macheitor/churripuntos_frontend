7570f7bc3ea05a2278f10392dd13069d5c84a780
Victor Machado <victor.machado.perez@gmail.com>
2023-07-27 20:51:37 +0200

    Build modal AcceptCancel

---
+++ src/components/ModalAcceptCancel.tsx
@@ -0,0 +1,62 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  Button,
+  useDisclosure,
+} from "@chakra-ui/react";
+import {  ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+  title: string;
+  cancelText?: string;
+  acceptText?: string;
+  onAccept: () => void;
+}
+
+const ModalAcceptCancel = ({
+  children,
+  title,
+  cancelText="Cancel",
+  acceptText="Accept",
+  onAccept,
+}: Props) => {
+  const { onOpen, onClose, isOpen } = useDisclosure();
+
+  return (
+    <>
+      <div onClick={onOpen}>{children}</div>
+
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
+        <ModalOverlay />
+
+        <ModalContent>
+          <ModalHeader>{title}</ModalHeader>
+
+          <ModalBody></ModalBody>
+
+          <ModalFooter>
+            <Button variant="outline" mr={3} onClick={onClose}>
+              {cancelText}
+            </Button>
+            <Button
+              onClick={() => {
+                onAccept();
+                onClose();
+              }}
+              colorScheme="red"
+            >
+              {acceptText}
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default ModalAcceptCancel;
