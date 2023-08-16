0769d3ec85c4f9e803aa722c6069acce5909ca19
Victor Machado <victor.machado.perez@gmail.com>
2023-08-16 15:27:03 +0200

    Delete unused component

--- src/components/SpaceCard.tsx
+++ src/components/SpaceCard.tsx
@@ -1,5 +1,4 @@
 import { Card, CardBody, Heading, Text, HStack } from "@chakra-ui/react";
-import Score from "./Score";
 import { Space } from "./Space"
 
 interface Props {
@@ -11,14 +10,6 @@ const SpaceCard = ({ space, onSelect }: Props) => {
     <Card borderRadius={10} overflow="hidden">
       <CardBody onClick={() => onSelect(space._id)}>
         <Heading fontSize="2xl">{space.spacename}</Heading>
-        {/* {space.users.map((u) => (
-          <HStack key={u._id}>
-            <Score score={100} />
-            <Text color={"gray.400"} fontSize="lg" marginY={2}>
-              {u.username}
-            </Text>
-          </HStack>
-        ))} */}
       </CardBody>
     </Card>
   );
