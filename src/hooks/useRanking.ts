779063eac2e308fac6daf02f2c6cf5be6348f6ad
Victor Machado <victor.machado.perez@gmail.com>
2023-08-21 15:41:33 +0200

    Ignore rank users that are not in the space

--- src/hooks/useRanking.ts
+++ src/hooks/useRanking.ts
@@ -2,18 +2,22 @@ import { Activity, User } from "../hooks/useSpace";
 
 export interface Rank extends User {
   points: number;
+  isUserInSpace: boolean;
 }
 
 const buildRanking = (users: User[], arr: Activity[]) => {
-  let result: Rank[] = users.map((user) => ({ ...user, points: 0 }));
+  let result: Rank[] = users.map((user) => ({
+    ...user,
+    points: 0,
+    isUserInSpace: true,
+  }));
 
   arr.forEach((activity) => {
     const index = result.findIndex((user) => user._id === activity.userId);
     if (index !== -1) {
       result[index].points += activity.points;
-    } else {
-      console.log("weird shit happened")
-    }
+    } 
+    // Ignore the users with activities that are not in the space anymore
   });
   return result;
 };
