d25882a7ae4a6543a99b34cf5fa653825fdb9ce2
Victor Machado <victor.machado.perez@gmail.com>
2023-08-23 01:09:47 +0200

    Revert Login and Register to previous version

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,25 +1,36 @@
 import {
   Flex,
   Heading,
+  Input,
+  Button,
+  InputGroup,
   Stack,
+  InputLeftElement,
+  chakra,
   Box,
   Link,
   Avatar,
-  Center,
+  FormControl,
+  FormHelperText,
   Text,
+  Center,
 } from "@chakra-ui/react";
-import { FieldValues } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
+import { FaUserAlt, FaLock } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import { CanceledError } from "../services/api-client";
 import { useState } from "react";
 import loginService, { LoginRequest } from "./../services/login-service";
-import Form from "./Form";
+
+const CFaUserAlt = chakra(FaUserAlt);
+const CFaLock = chakra(FaLock);
 
 const Login = () => {
-  const navigate = useNavigate();
   const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const { register, handleSubmit, reset } = useForm();
 
-  const onLogin = (data: FieldValues) => {
+  const onSubmit = (data: FieldValues) => {
     const loginRequest: LoginRequest = {
       email: data.email,
       password: data.password,
@@ -37,6 +48,8 @@ const Login = () => {
         if (err instanceof CanceledError) return;
         setError(err.response.data.message);
       });
+    // Reset the form inputs no matter what
+    reset();
   };
 
   return (
@@ -56,30 +69,60 @@ const Login = () => {
         <Avatar bg="teal.500" />
         <Heading color="teal.400">Churripuntos</Heading>
         <Box minW={{ base: "90%", md: "468px" }}>
-          <Form
-            acceptText="Login"
-            emailInput={true}
-            passwordInput={true}
-            onAccept={(data: FieldValues) => {
-              onLogin(data);
-            }}
-          />
-        </Box>
-        <Center>
-          {error && (
-            <Text as="i" color="red">
-              {error}
-            </Text>
-          )}
-        </Center>
-        <Link>Forgot password?</Link>
-        <Box>
-          Not registered yet?{" "}
-          <Link color="teal.500" onClick={() => navigate("/register")}>
-            Sign Up
-          </Link>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Stack spacing={4} p="1rem" boxShadow="md">
+              <FormControl>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents="none"
+                    children={<CFaUserAlt color="gray.300" />}
+                  />
+                  <Input
+                    {...register("email")}
+                    type="text"
+                    placeholder="Email address"
+                    required
+                  />
+                </InputGroup>
+              </FormControl>
+              <FormControl>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents="none"
+                    color="gray.300"
+                    children={<CFaLock color="gray.300" />}
+                  />
+                  <Input
+                    {...register("password")}
+                    type="password"
+                    placeholder="Password"
+                    required
+                  />
+                </InputGroup>
+                <Center>
+                  {error && (
+                    <Text as="i" color="red">
+                      {error}
+                    </Text>
+                  )}
+                </Center>
+                <FormHelperText textAlign="right">
+                  <Link>Forgot password?</Link>
+                </FormHelperText>
+              </FormControl>
+              <Button type="submit" colorScheme="teal">
+                Login
+              </Button>
+            </Stack>
+          </form>
         </Box>
       </Stack>
+      <Box>
+        Not registered yet?{" "}
+        <Link color="teal.500" onClick={() => navigate("/register")}>
+          Sign Up
+        </Link>
+      </Box>
     </Flex>
   );
 };
